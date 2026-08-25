4e3d2a7496fce5f34207921d3b0433deac24dfde
thevanburenboy <jamesbutler2021@gmail.com>
2026-08-25 11:20:09 -0400

    Initial commit after barebones creation/testing

--- src/main.ts
+++ src/main.ts
@@ -1,114 +1,258 @@
+import initSqlJs, { Database, SqlJsStatic } from 'sql.js';
 import {
-	Editor,
-	MarkdownView,
-	MarkdownFileInfo,
-	Modal,
+	ItemView,
 	Notice,
 	Plugin,
+	Setting,
+	WorkspaceLeaf,
 } from 'obsidian';
 import {
 	DEFAULT_SETTINGS,
-	MyPluginSettings,
-	SampleSettingTab,
+	CribbageTrackerSettings,
+	CribbageTrackerSettingTab,
 } from './settings';
 
-// Remember to rename these classes and interfaces!
+const VIEW_TYPE_CRIBBAGE = 'cribbage-tracker-view';
 
-export default class MyPlugin extends Plugin {
-	settings!: MyPluginSettings;
+class CribbageDatabase {
+	private sql: SqlJsStatic | null = null;
+	private db: Database | null = null;
+
+	constructor(private plugin: CribbageTrackerPlugin) {}
+
+	async load(): Promise<void> {
+        const wasmPath = `${this.plugin.manifest.dir}/sql-wasm.wasm`;
+
+        const wasmBinary =
+            await this.plugin.app.vault.adapter.readBinary(wasmPath);
+
+        this.sql = await initSqlJs({
+            wasmBinary,
+        });
+
+		const path = this.plugin.settings.databasePath;
+
+		if (await this.plugin.app.vault.adapter.exists(path)) {
+			const data = await this.plugin.app.vault.adapter.readBinary(path);
+
+			// Obsidian returns an ArrayBuffer; sql.js expects array-like data.
+			this.db = new this.sql.Database(new Uint8Array(data));
+		} else {
+			this.db = new this.sql.Database();
+
+			this.db.run(`
+				CREATE TABLE test (
+					id INTEGER PRIMARY KEY,
+					message TEXT NOT NULL
+				);
+			`);
+
+			this.db.run(
+				'INSERT INTO test (message) VALUES (?)',
+				['Hello from Cribbage Tracker!'],
+			);
+
+			await this.save();
+		}
+	}
+
+	async save(): Promise<void> {
+		if (!this.db) {
+			throw new Error('Database has not been loaded.');
+		}
+
+		const data = this.db.export();
+
+		const parent = this.plugin.settings.databasePath
+			.split('/')
+			.slice(0, -1)
+			.join('/');
+
+		if (
+			parent &&
+			!(await this.plugin.app.vault.adapter.exists(parent))
+		) {
+			await this.plugin.app.vault.createFolder(parent);
+		}
+
+		// Obsidian wants an ArrayBuffer; sql.js gives us a Uint8Array.
+		const buffer = data.buffer.slice(
+			data.byteOffset,
+			data.byteOffset + data.byteLength,
+		) as ArrayBuffer;
+
+		await this.plugin.app.vault.adapter.writeBinary(
+			this.plugin.settings.databasePath,
+			buffer,
+		);
+	}
+
+	getTestMessage(): string {
+		if (!this.db) {
+			throw new Error('Database has not been loaded.');
+		}
+
+		const result = this.db.exec(
+			'SELECT message FROM test LIMIT 1',
+		);
+
+		const firstResult = result[0];
+
+		if (!firstResult) {
+			return 'No results found.';
+		}
+
+		const firstRow = firstResult.values[0];
+
+		if (!firstRow) {
+			return 'No message found.';
+		}
+
+		const message = firstRow[0];
+
+		if (message === undefined || message === null) {
+			return 'No message found.';
+		}
+
+		return String(message);
+	}
+}
+
+export default class CribbageTrackerPlugin extends Plugin {
+	settings!: CribbageTrackerSettings;
+	database!: CribbageDatabase;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (_evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
+		this.database = new CribbageDatabase(this);
+		await this.database.load();
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			},
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (
-				editor: Editor,
-				_ctx: MarkdownView | MarkdownFileInfo,
-			) => {
-				editor.replaceSelection('Sample editor command');
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
-			},
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(activeDocument, 'click', (_evt: MouseEvent) => {
-			new Notice('Click');
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000),
+		this.registerView(
+			VIEW_TYPE_CRIBBAGE,
+			(leaf) => new CribbageTrackerView(leaf, this),
 		);
+
+		this.addRibbonIcon('dice-5', 'Open Cribbage Tracker', () => {
+			this.activateView();
+		});
+
+		this.addCommand({
+			id: 'open-cribbage-tracker',
+			name: 'Open Cribbage Tracker',
+			callback: () => {
+				this.activateView();
+			},
+		});
+
+		this.addSettingTab(
+			new CribbageTrackerSettingTab(this.app, this),
+		);
+
+		console.log('Cribbage Tracker loaded');
 	}
 
-	onunload() {}
+	onunload() {
+		this.app.workspace.detachLeavesOfType(VIEW_TYPE_CRIBBAGE);
+		console.log('Cribbage Tracker unloaded');
+	}
 
 	async loadSettings() {
 		this.settings = Object.assign(
 			{},
 			DEFAULT_SETTINGS,
-			(await this.loadData()) as Partial<MyPluginSettings>,
+			await this.loadData(),
 		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async activateView() {
+		const { workspace } = this.app;
+
+		let leaf = workspace.getLeavesOfType(VIEW_TYPE_CRIBBAGE)[0];
+
+		if (!leaf) {
+			leaf = workspace.getLeaf('tab');
+
+			await leaf.setViewState({
+				type: VIEW_TYPE_CRIBBAGE,
+				active: true,
+			});
+		}
+
+		workspace.revealLeaf(leaf);
+	}
 }
 
-class SampleModal extends Modal {
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
+class CribbageTrackerView extends ItemView {
+	constructor(
+		leaf: WorkspaceLeaf,
+		private plugin: CribbageTrackerPlugin,
+	) {
+		super(leaf);
 	}
 
-	onClose() {
+	getViewType(): string {
+		return VIEW_TYPE_CRIBBAGE;
+	}
+
+	getDisplayText(): string {
+		return 'Cribbage Tracker';
+	}
+
+	getIcon(): string {
+		return 'dice-5';
+	}
+
+	async onOpen() {
+		this.render();
+	}
+
+	async onClose() {}
+
+	render() {
 		const { contentEl } = this;
+
 		contentEl.empty();
+
+		contentEl.createEl('h1', {
+			text: 'Cribbage Tracker',
+		});
+
+		contentEl.createEl('p', {
+			text: 'Database test',
+		});
+
+		const message = contentEl.createEl('p', {
+			text: this.plugin.database.getTestMessage(),
+		});
+
+		new Setting(contentEl)
+			.setName('Database path')
+			.setDesc(this.plugin.settings.databasePath);
+
+		const button = contentEl.createEl('button', {
+			text: 'Reload Database',
+		});
+
+		button.addEventListener('click', async () => {
+			try {
+				await this.plugin.database.load();
+
+				message.setText(
+					this.plugin.database.getTestMessage(),
+				);
+
+				new Notice('Database loaded successfully.');
+			} catch (error) {
+				console.error(error);
+				new Notice(
+					'Failed to load database. Check the console.',
+				);
+			}
+		});
 	}
 }
--- src/settings.ts
+++ src/settings.ts
@@ -1,38 +1,44 @@
 import { App, PluginSettingTab, Setting } from 'obsidian';
-import MyPlugin from './main';
+import CribbageTrackerPlugin from './main';
 
-export interface MyPluginSettings {
-	mySetting: string;
+export interface CribbageTrackerSettings {
+    databasePath: string;
 }
 
-export const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default',
+export const DEFAULT_SETTINGS: CribbageTrackerSettings = {
+    databasePath: 'Cribbage/cribbage.db',
 };
 
-export class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+export class CribbageTrackerSettingTab extends PluginSettingTab {
+    plugin: CribbageTrackerPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    constructor(app: App, plugin: CribbageTrackerPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	display(): void {
-		const { containerEl } = this;
+    display(): void {
+        const { containerEl } = this;
 
-		containerEl.empty();
+        containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Settings #1')
-			.setDesc("It's a secret")
-			.addText((text) =>
-				text
-					.setPlaceholder('Enter your secret')
-					.setValue(this.plugin.settings.mySetting)
-					.onChange(async (value) => {
-						this.plugin.settings.mySetting = value;
-						await this.plugin.saveSettings();
-					}),
-			);
-	}
+        containerEl.createEl('h2', {
+            text: 'Cribbage Tracker Settings',
+        });
+
+        new Setting(containerEl)
+            .setName('Database path')
+            .setDesc(
+                'Path to the SQLite database, relative to the root of your vault.',
+            )
+            .addText((text) =>
+                text
+                    .setPlaceholder('Cribbage/cribbage.db')
+                    .setValue(this.plugin.settings.databasePath)
+                    .onChange(async (value) => {
+                        this.plugin.settings.databasePath = value.trim();
+                        await this.plugin.saveSettings();
+                    }),
+            );
+    }
 }
